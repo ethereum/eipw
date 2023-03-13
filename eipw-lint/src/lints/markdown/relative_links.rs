02b7d5d5d42885c02b55ac6ccf2570fd43547ae5
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2023-03-13 03:28:18 +0100

    Update relative_links.rs

--- eipw-lint/src/lints/markdown/relative_links.rs
+++ eipw-lint/src/lints/markdown/relative_links.rs
@@ -48,11 +48,9 @@ impl<'e> Lint for RelativeLinks<'e> {
             let line_with_address = ctx.line(line_start);
             let line_link_regx = re.captures(line_with_address.as_bytes()).unwrap();
             let line_link_address = str::from_utf8(&line_link_regx[0]).unwrap();
-          
-            
+                
             write!(link_md, "`{}`",&line_link_address).unwrap();
-             
-            
+              
             let mut footer_label = String::new();
             let mut footer = vec![];
             

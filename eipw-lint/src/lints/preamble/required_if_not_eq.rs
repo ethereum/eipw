2abbd647d1338ef37e01c283f306fd8f4c487510
Gavin John <gavinnjohn@gmail.com>
2023-03-22 15:01:27 -0400

    Create required_if_not_eq.rs

---
+++ eipw-lint/src/lints/preamble/required_if_not_eq.rs
@@ -0,0 +1,141 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use annotate_snippets::snippet::{Annotation, AnnotationType, Slice, Snippet, SourceAnnotation};
+
+use crate::lints::{Context, Error, Lint};
+
+#[derive(Debug)]
+pub struct RequiredIfNotEq<'b> {
+    pub when: &'b str,
+    pub notEquals: &'b str,
+    pub then: &'b str,
+}
+
+impl<'n> Lint for RequiredIfNotEq<'n> {
+    fn lint<'a, 'b>(&self, slug: &'a str, ctx: &Context<'a, 'b>) -> Result<(), Error> {
+        let then_opt = ctx.preamble().by_name(self.then);
+        let when_opt = ctx.preamble().by_name(self.when);
+
+        match (when_opt, then_opt) {
+            // Correct.
+            (None, None) => (),
+
+            // Correct.
+            (Some(when), Some(_)) if when.value().trim() != self.notEquals => (),
+
+            // Correct.
+            (Some(when), None) if when.value().trim() != self.notEquals => (),
+
+            // Incorrect.
+            (Some(when), None) => {
+                let label = format!(
+                    "preamble header `{}` is required when `{}` is not `{}`",
+                    self.then, self.when, self.notEquals,
+                );
+                ctx.report(Snippet {
+                    title: Some(Annotation {
+                        annotation_type: ctx.annotation_type(),
+                        id: Some(slug),
+                        label: Some(&label),
+                    }),
+                    footer: vec![],
+                    slices: vec![Slice {
+                        line_start: when.line_start(),
+                        fold: false,
+                        origin: ctx.origin(),
+                        source: when.source(),
+                        annotations: vec![SourceAnnotation {
+                            annotation_type: AnnotationType::Info,
+                            label: "defined here",
+                            range: (0, when.source().len()),
+                        }],
+                    }],
+                    opt: Default::default(),
+                })?;
+            }
+
+            // Incorrect.
+            (Some(when), Some(then)) => {
+                let label = format!(
+                    "preamble header `{}` is only allowed when `{}` is not `{}`",
+                    self.then, self.when, self.notEquals,
+                );
+
+                let info_label = format!("unless equal to `{}`", self.notEquals);
+
+                let mut slices = vec![
+                    Slice {
+                        line_start: when.line_start(),
+                        fold: false,
+                        origin: ctx.origin(),
+                        source: when.source(),
+                        annotations: vec![SourceAnnotation {
+                            annotation_type: AnnotationType::Info,
+                            label: &info_label,
+                            range: (0, when.source().len()),
+                        }],
+                    },
+                    Slice {
+                        line_start: then.line_start(),
+                        fold: false,
+                        origin: ctx.origin(),
+                        source: then.source(),
+                        annotations: vec![SourceAnnotation {
+                            annotation_type: ctx.annotation_type(),
+                            label: "remove this",
+                            range: (0, then.source().len()),
+                        }],
+                    },
+                ];
+
+                slices.sort_by_key(|s| s.line_start);
+
+                ctx.report(Snippet {
+                    title: Some(Annotation {
+                        annotation_type: ctx.annotation_type(),
+                        id: Some(slug),
+                        label: Some(&label),
+                    }),
+                    footer: vec![],
+                    slices,
+                    opt: Default::default(),
+                })?;
+            }
+
+            // Incorrect.
+            (None, Some(then)) => {
+                let label = format!(
+                    "preamble header `{}` is only allowed when `{}` is not `{}`",
+                    self.then, self.when, self.notEquals,
+                );
+
+                ctx.report(Snippet {
+                    title: Some(Annotation {
+                        annotation_type: ctx.annotation_type(),
+                        id: Some(slug),
+                        label: Some(&label),
+                    }),
+                    footer: vec![],
+                    slices: vec![Slice {
+                        line_start: then.line_start(),
+                        fold: false,
+                        origin: ctx.origin(),
+                        source: then.source(),
+                        annotations: vec![SourceAnnotation {
+                            annotation_type: ctx.annotation_type(),
+                            label: "defined here",
+                            range: (0, then.source().len()),
+                        }],
+                    }],
+                    opt: Default::default(),
+                })?;
+            }
+        }
+
+        Ok(())
+    }
+}

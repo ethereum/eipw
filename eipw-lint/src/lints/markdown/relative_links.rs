99194deeafd792fc943a035f22a0e67753081974
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2023-04-03 01:20:46 +0200

    Update relative_links.rs

--- eipw-lint/src/lints/markdown/relative_links.rs
+++ eipw-lint/src/lints/markdown/relative_links.rs
@@ -45,27 +45,42 @@ impl<'e> Lint for RelativeLinks<'e> {
             .filter(|l| re.is_match(&l.address) && !exceptions.is_match(&l.address));
 
         for Link { line_start, .. } in links {
-            let mut line_link_regx : Vec<u8> = Vec::new();
             
+            //let mut line_link_regx : Vec<u8> = Vec::new();
             let mut footer_label = String::new();
-            
             let mut footer = vec![];
-            
             let mut link_md = String::new();
             
             let line_with_address = ctx.line(line_start);
             
+            //let line_link_regx = re.captures(line_with_address.as_bytes()).unwrap();
             
-            if (line_with_address != "/") && (line_with_address != "://") && (line_with_address !="") {
+            match re.captures(line_with_address.as_bytes()) {
+                Some(caps) => {
+                    
+                    let line_link_address = str::from_utf8(&caps[0]).unwrap(); //caps.get(1).unwrap().as_str();
+                    write!(link_md, "`{}`",&line_link_address).unwrap();  //println!("{}", cap);
+                    
+                    if !(ctx.line(line_start).contains(&link_md)) {
+                        
+                        write!(footer_label, "\n use: {} instead \n",&link_md,).unwrap();
+                
+                        footer.push(Annotation {
+                            annotation_type: AnnotationType::Help,
+                            id: None,
+                            label: Some(&footer_label),
+                        });   
+                    }            
+                }
+                None => {
+                        todo!(); // The regex did not match. Deal with it here!
+                }
+            }
             
-            let line_link_regx = re.captures(line_with_address.as_bytes()).unwrap();
-            //let text1 = str::from_utf8(&line_link_regx[0]).unwrap(); //line_link_regx.get(0).map_or("", |m| m.as_str());
-            //assert_eq!(text1,""); 
             
+      /*      if line_link_regx.len() >1 {
             
-            if line_link_regx.len() >1 {
-            
-                let line_link_address = str::from_utf8(&line_link_regx[0]).unwrap();
+                //let line_link_address = str::from_utf8(&line_link_regx[0]).unwrap();
                 
                 write!(link_md, "`{}`",&line_link_address).unwrap();
                 
@@ -77,9 +92,9 @@ impl<'e> Lint for RelativeLinks<'e> {
                         id: None,
                         label: Some(&footer_label),
                      });   
-                }
-            }
-            }
+                }*/
+            
+            
             
             ctx.report(Snippet {
                 title: Some(Annotation {

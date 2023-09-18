8d51feb6db57f7f3be0efa1183e9857aef32b2d0
prampey <prudhvi.rampey@gmail.com>
2023-09-17 22:46:46 -0400

    Add annotation

--- eipw-lint/src/lints/markdown/headings_space.rs
+++ eipw-lint/src/lints/markdown/headings_space.rs
@@ -6,68 +6,68 @@
 
 use annotate_snippets::snippet::{Annotation, Slice, Snippet};
 
+use annotate_snippets::snippet::SourceAnnotation;
 use comrak::nodes::Ast;
 use comrak::nodes::NodeValue;
 
 use crate::lints::{Context, Error, Lint};
-use crate::reporters::text;
-use crate::tree::{self, Next, TraverseExt};
-
-use regex::{Regex, RegexSet};
-
-use scraper::node::Node as HtmlNode;
-use scraper::Html;
 
 use serde::{Deserialize, Serialize};
 
-use snafu::Snafu;
-
-use std::fmt::{Debug, Display};
+use std::fmt::Debug;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct HeadingsSpace;
 
 impl Lint for HeadingsSpace {
     fn lint<'a, 'b>(&self, slug: &'a str, ctx: &Context<'a, 'b>) -> Result<(), Error> {
-        // Collect all headings.
         let false_headings: Vec<_> = ctx
-        .body()
-        .descendants()
-        .filter_map(|node| match &*node.data.borrow() { 
-            Ast { value: NodeValue::Text(text), .. } => {
-                if text.starts_with("#") {
-                    Some((text.clone(), node.data.borrow().sourcepos.start.line))
+            .body()
+            .descendants()
+            .filter_map(|node| match &*node.data.borrow() {
+                // Collect all Text nodes as Markdown does not recognise headings without space
+                Ast {
+                    value: NodeValue::Text(text),
+                    ..
+                } => {
+                    if text.starts_with("#") {
+                        Some((text.clone(), node.data.borrow().sourcepos.start.line))
+                    } else {
+                        None
+                    }
                 }
-                else {
-                    None
+                _ => None,
+            })
+            .collect();
+
+        let slices = false_headings
+            .iter()
+            .map(|(text, line_start)| {
+                let error_idx = text.rfind("#").unwrap();
+                Slice {
+                    line_start: line_start.clone(),
+                    origin: ctx.origin(),
+                    source: text,
+                    fold: false,
+                    annotations: vec![SourceAnnotation {
+                        annotation_type: ctx.annotation_type(),
+                        label: "space required here",
+                        range: (error_idx, error_idx + 1),
+                    }],
                 }
-            },
-            _ => None,
-        }).collect();
+            })
+            .collect();
 
-        let slices = false_headings.iter()
-        .map(|(text, line_start)| {
-            Slice{
-                line_start: line_start.clone(),
-                origin: ctx.origin(),
-                source: text,
-                fold: false,
-                annotations: vec![],
-            }
-        })
-        .collect();
-
-        // Print all false headings
         ctx.report(Snippet {
             title: Some(Annotation {
-                    id: Some(slug),
-                    annotation_type: ctx.annotation_type(),
-                    label: Some("Space missing in header"),
-                }),
-                footer: vec![],
-                slices,
-                opt: Default::default(),
-            })?;
+                id: Some(slug),
+                annotation_type: ctx.annotation_type(),
+                label: Some("Space missing in header"),
+            }),
+            footer: vec![],
+            slices,
+            opt: Default::default(),
+        })?;
         Ok(())
     }
 }

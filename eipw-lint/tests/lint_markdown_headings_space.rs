ace9daa5032af662fb9f7eec7f9acc43168cfd7b
prampey <prudhvi.rampey@gmail.com>
2023-09-19 18:44:23 -0400

    Add tests

---
+++ eipw-lint/tests/lint_markdown_headings_space.rs
@@ -0,0 +1,71 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use eipw_lint::lints::markdown::HeadingsSpace;
+use eipw_lint::reporters::Text;
+use eipw_lint::Linter;
+
+#[tokio::test]
+async fn normal_headings() {
+    let src = r#"---
+header: value1
+---
+
+##Banana
+####Mango
+"#;
+
+    let reports = Linter::<Text<String>>::default()
+        .clear_lints()
+        .deny("markdown-headings-space", HeadingsSpace{})
+        .check_slice(None, src)
+        .run()
+        .await
+        .unwrap()
+        .into_inner();
+
+    assert_eq!(
+        reports,
+        r#"error[markdown-headings-space]: Space missing in header
+  |
+5 | ##Banana
+  |  ^ space required here
+  |
+6 | ####Mango
+  |    ^ space required here
+  |
+"#
+    );
+}
+
+#[tokio::test]
+async fn abnormal_heading() {
+    let src = r#"---
+header: value1
+---
+
+##B#an#ana
+"#;
+
+    let reports = Linter::<Text<String>>::default()
+        .clear_lints()
+        .deny("markdown-headings-space", HeadingsSpace{})
+        .check_slice(None, src)
+        .run()
+        .await
+        .unwrap()
+        .into_inner();
+
+    assert_eq!(
+        reports,
+        r#"error[markdown-headings-space]: Space missing in header
+  |
+5 | ##B#an#ana
+  |  ^ space required here
+  |
+"#
+    );
+}

1e8970398f245795c719fa260509c6fefeaa9e86
Abeeujah <abeeujah@gmail.com>
2024-11-04 16:08:11 +0100

    refac: Implemented reviewed changes to maintain consistency and improve error message ergonomics.

--- eipw-lint/src/lints/markdown/heading_first.rs
+++ eipw-lint/src/lints/markdown/heading_first.rs
@@ -5,26 +5,31 @@
  */
 
 use comrak::nodes::NodeValue;
-use eipw_snippets::Level;
 
 use crate::lints::{Error, Lint};
 
 #[derive(Debug)]
-pub struct HeadingsOnly;
+pub struct HeadingFirst;
 
-impl Lint for HeadingsOnly {
+impl Lint for HeadingFirst {
     fn lint<'a>(&self, slug: &'a str, ctx: &crate::lints::Context<'a, '_>) -> Result<(), Error> {
-        let annotation_type = Level::Error;
         let second = match ctx.body().descendants().nth(1) {
             Some(el) => el.data.borrow().to_owned().value,
-            None => return ctx.report(annotation_type.title("Cannot submit an empty proposal")),
+            None => {
+                return ctx.report(
+                    ctx.annotation_level()
+                        .title("Cannot submit an empty proposal")
+                        .id(slug),
+                )
+            }
         };
         match second {
             NodeValue::Heading(_) => Ok(()),
-            _ => {
-                let annotation_type = Level::Error;
-                ctx.report(annotation_type.title("Only Heading is allowed after FrontMatter"))
-            }
+            _ => ctx.report(
+                ctx.annotation_level()
+                    .title("Nothing is permitted between the preamble and the first heading")
+                    .id(slug),
+            ),
         }
     }
 }
